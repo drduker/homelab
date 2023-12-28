1a3f8367f97e7df2ea1cd42b10ab95c993b290a8
Lucas Pick <lucas.pick@secondfront.com>
2023-12-28 15:49:00 -0600

    remove smtp-relay

--- infrastructure/talos/apply.sh
+++ infrastructure/talos/apply.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # Deploy the configuration to the nodes
-talosctl apply-config -n 192.168.1.83 -f ./clusterconfig/pickhaus-tiny-001.internal.pick.haus.yaml
+talosctl apply-config -n 192.168.1.132 -f ./clusterconfig/pickhaus-tiny-001.internal.pick.haus.yaml
 talosctl apply-config -n 192.168.1.115 -f ./clusterconfig/pickhaus-tiny-002.internal.pick.haus.yaml
--- infrastructure/talos/bootstrap.sh
+++ infrastructure/talos/bootstrap.sh
@@ -2,13 +2,13 @@
 
 echo "Applying Node Configs"
 # Deploy the configuration to the nodes
-talosctl apply-config -i -n 192.168.1.83 -f ./clusterconfig/pickhaus-tiny-001.internal.pick.haus.yaml
+talosctl apply-config -i -n 192.168.1.132 -f ./clusterconfig/pickhaus-tiny-001.internal.pick.haus.yaml
 talosctl apply-config -i -n 192.168.1.115 -f ./clusterconfig/pickhaus-tiny-002.internal.pick.haus.yaml
 
 echo "Sleeping..."
 sleep 120
 
-talosctl config node "192.168.1.83"; talosctl config endpoint 192.168.1.83 192.168.1.115
+talosctl config node "192.168.1.132"; talosctl config endpoint 192.168.1.132 192.168.1.115
 echo "Running bootstrap..."
 talosctl bootstrap
 
